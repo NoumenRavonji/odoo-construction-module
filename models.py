6267c61065e4ce21254410b4fbe8267e0f606801
Noumen Ravonji <r.nomena.sitraka@gmail.com>
2018-01-25 10:06:00 +0300

    fix avant metre relation

--- models.py
+++ models.py
@@ -4,14 +4,26 @@ from openerp import models, fields, api
 
 
 class AvantMetre(models.Model):
-	_name ='gent.avantmetre'
 	_inherit = 'mrp.bom'
+	_name = "gent.avantmetre"
 
+	bom_line_ids =  fields.One2many('gent.avantmetre.line', 'bom_id', 'BoM Lines', copy=True)
 	rubrique = fields.Char(string="Rubrique")
 	nom = fields.Char(string="Nom")
 	prix_total = fields.Float(string="Prix Total")
+	state = fields.Selection([
+        ('simple', "Simple"),
+        ('avant_metre', "Avant Métré"),
+        ('sous_detail', "Sous détail"),
+    ], default='simple')
 
-	# @api.model
-	# def create(self,values):
-	# 	print values
-	# 	return super(AvantMetre, self).create(values)
+class AvantMetreLine(models.Model):
+	_inherit = "mrp.bom.line"
+	_name = 'gent.avantmetre.line'
+
+	bom_id =  fields.Many2one('gent.avantmetre', 'Parent BoM', ondelete='cascade', select=True, required=True)
+
+
+
+
+
