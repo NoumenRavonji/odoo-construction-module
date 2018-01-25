81b5416bbcbe45dcdb6dd8c3c1972362b718a5f1
Atrika <8randriamanalina8@gmail.com>
2018-01-25 13:47:19 +0300

    avant metre jeudi 25

--- models.py
+++ models.py
@@ -11,11 +11,11 @@ class AvantMetre(models.Model):
 	nom = fields.Char(string="Nom")
 	prix_total = fields.Float(string="Prix Total")
 	
-	rubrique_ids =  fields.One2many(comodel_name='gent.avantmetre.rubrique', inverse_name='avantmetre_bom_id', copy=True)
+	# rubrique_ids =  fields.One2many(comodel_name='gent.avantmetre.rubrique', inverse_name='avantmetre_bom_id', copy=True)
 	
 
-class Rubrique(models.Model):
-	_name='gent.avantmetre.rubrique'
-	name = fields.Char(string="Rubrique")
-	avantmetre_bom_id = fields.Many2one(comodel_name='gent.avantmetre')
-	avantmetre_bom_line_ids = fields.One2many('mrp.bom.line', 'bom_id', string='BoM Lines')
+# class Rubrique(models.Model):
+# 	_name='gent.avantmetre.rubrique'
+# 	name = fields.Char(string="Rubrique")
+# 	avantmetre_bom_id = fields.Many2one(comodel_name='gent.avantmetre')
+# 	avantmetre_bom_line_ids = fields.One2many('mrp.bom.line', 'bom_id', string='BoM Lines')
