c08ea2a7082a40aa914c8e56b8ab7857fbb0cef5
Noumen Ravonji <r.nomena.sitraka@gmail.com>
2018-01-26 09:56:02 +0300

    simple commit

--- models.py
+++ models.py
@@ -15,6 +15,32 @@ class AvantMetre(models.Model):
         ('avant_metre', "Avant Métré"),
         ('sous_detail', "Sous détail"),
     ], default='simple')
+	
+	@api.onchange('bom_line_ids')
+	def on_change_bom_line_ids(self):
+		print "CHANGE"
+		i = 0
+		rubrique_str = ""
+		# for line in bom_line_ids:
+		# 	print line
+		# 	if(i == 1):
+		# 		rubrique_str = line[2]['rubrique']
+
+		# 	i = i+1
+		# 	print line
+
+		print "CHANGE LOOP"
+		print self.bom_line_ids
+
+		for line in self.bom_line_ids:
+			print line
+			if(i == 1):
+				rubrique_str = line[2]['rubrique']
+
+		print rubrique_str
+
+
+		pass
 
 class AvantMetreLine(models.Model):
 	_inherit = "mrp.bom.line"
